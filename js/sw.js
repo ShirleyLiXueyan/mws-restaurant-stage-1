ee743d0d069aa3dc94e67d97251e17bb7aa1fb5c
lixueyan <421079462@qq.com>
2018-07-18 22:57:33 +0800

    initial commit

---
+++ js/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('fetch', function(event) {
+  console.log(event.request);
+});
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
