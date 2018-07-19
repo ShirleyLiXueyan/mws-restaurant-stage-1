ba3880b59a99a362a3b233626b70ec446be35e3b
lixueyan <421079462@qq.com>
2018-07-19 22:28:37 +0800

    modify

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/skeleton',
+        //'/skeleton',
         'js/main.js',
         'js/restaurant_info.js',
         'js/dbhelper.js',
